e34e84777de14cd70da8a5817363b0fcbac91cf5
Lm09 <123.moharana@gmail.com>
2025-10-26 00:14:27 +0530

    add: leftnav redirections and light/dark theme toggler

--- client/src/components/LeftNav.tsx
+++ client/src/components/LeftNav.tsx
@@ -3,35 +3,69 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Home, MessageCircle, Settings, User, Workflow } from "lucide-react";
+import {
+  Home,
+  ListPlus,
+  MessageCircle,
+  Moon,
+  Settings,
+  Sun,
+  User,
+  Workflow,
+} from "lucide-react";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "@/provider/theme-provider";
+import { useNavigate } from "react-router-dom";
 
 const LeftNav = () => {
+  const { isDark, toggleTheme } = useTheme();
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col w-[4%] h-screen bg-light-primary px-4 py-4 space-y-4 justify-between">
       <div className="w-full flex-col space-y-4">
         <ToolTipIcon
           icon={<Home className="w-10 h-10 text-text-light-primary" />}
           tooltip="Home"
+          onClick={() => navigate("/")}
         />
         <ToolTipIcon
           icon={<Workflow className="w-10 h-10 text-text-light-primary" />}
           tooltip="Workflow"
+          onClick={() => navigate("/workflow")}
+        />
+        <ToolTipIcon
+          icon={<ListPlus className="w-10 h-10 text-text-light-primary" />}
+          tooltip="Templates"
+          onClick={() => navigate("/templates")}
         />
         <ToolTipIcon
           icon={<MessageCircle className="w-10 h-10 text-text-light-primary" />}
           tooltip="AutoBot"
+          onClick={() => navigate("/ai/autobot")}
         />
       </div>
       <div className="w-full flex-col space-y-4">
         <ToolTipIcon
+          icon={
+            isDark ? (
+              <Sun className="w-10 h-10 text-light-primary" />
+            ) : (
+              <Moon className="w-10 h-10" />
+            )
+          }
+          tooltip={isDark ? "Light Mode" : "Dark Mode"}
+          onClick={toggleTheme}
+        />
+        <ToolTipIcon
           icon={<Settings className="w-10 h-10 text-text-light-primary" />}
           tooltip="Settings"
+          onClick={() => navigate("/settings")}
         />
         <ToolTipIcon
           icon={<User className="w-10 h-10 text-text-light-primary" />}
           tooltip="Account"
+          onClick={() => navigate("/account")}
         />
       </div>
     </div>
@@ -43,14 +77,20 @@ export default LeftNav;
 const ToolTipIcon = ({
   icon,
   tooltip,
+  onClick,
 }: {
   icon: React.ReactElement;
   tooltip: string;
+  onClick?: () => void;
 }) => {
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Button variant="outline" className="bg-gray-400/20 rounded-lg p-2">
+        <Button
+          variant="outline"
+          className="bg-gray-400/20 rounded-lg p-2"
+          onClick={onClick}
+        >
           {icon}
         </Button>
       </TooltipTrigger>
