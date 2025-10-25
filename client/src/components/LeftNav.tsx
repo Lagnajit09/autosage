6214fd91f0e9daa4098b7e88d6b06d6719df9e91
Lm09 <123.moharana@gmail.com>
2025-10-25 23:59:36 +0530

    add: leftnav ui

--- client/src/components/LeftNav.tsx
+++ client/src/components/LeftNav.tsx
@@ -1,14 +1,62 @@
-import { Home, MessageCircle, Workflow } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Home, MessageCircle, Settings, User, Workflow } from "lucide-react";
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const LeftNav = () => {
   return (
-    <div className="flex-col h-screen my-auto px-4 items-center space-y-4 bg-light-primary">
-      <Home className="w-10 h-10 text-text-light-primary bg-gray-400/20 rounded-lg p-2" />
-      <Workflow className="w-10 h-10 text-text-light-primary bg-gray-400/20 rounded-lg p-2" />
-      <MessageCircle className="w-10 h-10 text-text-light-primary bg-gray-400/20 rounded-lg p-2" />
+    <div className="flex flex-col w-[4%] h-screen bg-light-primary px-4 py-4 space-y-4 justify-between">
+      <div className="w-full flex-col space-y-4">
+        <ToolTipIcon
+          icon={<Home className="w-10 h-10 text-text-light-primary" />}
+          tooltip="Home"
+        />
+        <ToolTipIcon
+          icon={<Workflow className="w-10 h-10 text-text-light-primary" />}
+          tooltip="Workflow"
+        />
+        <ToolTipIcon
+          icon={<MessageCircle className="w-10 h-10 text-text-light-primary" />}
+          tooltip="AutoBot"
+        />
+      </div>
+      <div className="w-full flex-col space-y-4">
+        <ToolTipIcon
+          icon={<Settings className="w-10 h-10 text-text-light-primary" />}
+          tooltip="Settings"
+        />
+        <ToolTipIcon
+          icon={<User className="w-10 h-10 text-text-light-primary" />}
+          tooltip="Account"
+        />
+      </div>
     </div>
   );
 };
 
 export default LeftNav;
+
+const ToolTipIcon = ({
+  icon,
+  tooltip,
+}: {
+  icon: React.ReactElement;
+  tooltip: string;
+}) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline" className="bg-gray-400/20 rounded-lg p-2">
+          {icon}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{tooltip}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
