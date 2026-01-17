985442df89e4757a3e172425d8c3ee03cbf01062
Lm09 <123.moharana@gmail.com>
2026-01-17 14:58:32 +0530

    add: user-reference to workflows model

--- server/workflows/models.py
+++ server/workflows/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 import uuid
 
 
 class Workflow(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='workflows', null=True, blank=True)
     name = models.CharField(max_length=255)
     nodes = models.JSONField(default=list, blank=True)
     edges = models.JSONField(default=list, blank=True)
